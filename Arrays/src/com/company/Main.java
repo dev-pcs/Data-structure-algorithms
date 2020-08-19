127d44c6e4526d1c0f210845718dba5aa0757388
Priyank Shah <priyankshah@MacBook-Pro.attlocal.net>
2020-08-18 21:16:31 -0500

    create array

---
+++ Arrays/src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Array numbers = new Array(3);
+
+        numbers.insert(10);
+        numbers.insert(20);
+        numbers.insert(30);
+        numbers.insert(60);
+        numbers.insert(40);
+        
+        System.out.println(numbers.indexOf(600));
+    }
+}

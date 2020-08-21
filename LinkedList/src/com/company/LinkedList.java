6bd4b2c77780b44264e5b195f10952444ea9f660
Priyank Shah <priyankshah@MacBook-Pro.attlocal.net>
2020-08-21 08:41:39 -0500

    create reverse LinkedList function

--- LinkedList/src/com/company/LinkedList.java
+++ LinkedList/src/com/company/LinkedList.java
@@ -1,12 +1,12 @@
 package com.company;
 
+import java.util.NoSuchElementException;
+
 public class LinkedList {
 
     private class Node {
-
         private int value;
         private Node next;
-
         public Node(int value) {
             this.value = value;
         }
@@ -14,16 +14,118 @@ public class LinkedList {
 
     private Node first;
     private Node last;
+    private int size;
 
     public void addLast(int item) {
         var node  = new Node(item);
         node.next = null;
 
-        if (first == null)
+        if (isEmpty())
             first = last = node;
         else {
             last.next = node;
             last = node;
         }
+        size++;
     }
-}
+
+    public void addFirst(int item) {
+        var node = new Node(item);
+        if (isEmpty()) {
+            first = last = node;
+        }else {
+            node.next = first;
+            first = node;
+        }
+        size++;
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    public void deleteFirst() {
+        if (isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last) {
+            first = last = null;
+        }else {
+            var second = first.next;
+            first.next = null;
+            first = second;
+        }
+        size--;
+    }
+
+    public void deleteLast() {
+        if(isEmpty())
+            throw new NoSuchElementException();
+
+        if (first == last) {
+            first = last = null;
+        }else {
+            var previous = getPrevious(last);
+            last = previous;
+            last.next = null;
+        }
+        size--;
+    }
+
+    public int indexOf(int item) {
+        int index = 0;
+        var current = first;
+        while (current != null) {
+            if (current.value == item)
+                return index;
+            current = current.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public boolean contains(int item) {
+        return indexOf(item) != -1;
+    }
+
+    private Node getPrevious(Node node) {
+        var current = first;
+        while (current != null) {
+            if (current.next == node) return current;
+            current = current.next;
+        }
+        return null;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int[] toArray() {
+        int[] array = new int[size];
+        var current = first;
+        var index = 0;
+        while (current!= null) {
+            array[index++] = current.value;
+            current = current.next;
+        }
+        return array;
+    }
+
+    public void reverse() {
+
+        if (isEmpty()) return;
+        var previous = first;
+        var current = first.next;
+        while (current != null) {
+            var next = current.next;
+            current.next = previous;
+            previous = current;
+            current = next;
+        }
+
+        last = first;
+        last.next = null;
+        first = previous;
+    }
+    }
--- LinkedList/src/com/company/Main.java
+++ LinkedList/src/com/company/Main.java
@@ -1,12 +1,25 @@
 package com.company;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
         var list = new LinkedList();
+        System.out.println(list.size());
         list.addLast(3);
         list.addLast(4);
         list.addLast(5);
-        list.addLast(6);
+
+        System.out.println(list.size());
+        list.reverse();
+//        System.out.println(list.size());
+
+//        list.deleteLast();
+//        list.deleteFirst();
+//
+////        System.out.println(list.size());
+        var array = list.toArray();
+        System.out.println(Arrays.toString(array));
     }
 }
